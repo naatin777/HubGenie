1ef9443e83f908c25fbd36cf3583f22b54a8c614
Naatin <76672482+naatin777@users.noreply.github.com>
2025-10-25 23:46:53 +0900

    Add command name and description

--- src/hubgenie.ts
+++ src/hubgenie.ts
@@ -1,8 +1,8 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Command } from "@cliffy/command";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  await new Command()
+    .name("hubgenie")
+    .description("AI-powered CLI for generating smart commit messages, pull requests, and issues.")
+    .parse(Deno.args);
 }
