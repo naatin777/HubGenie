c56e84e140178749714687c25f94f72597a2d282
Naatin <76672482+naatin777@users.noreply.github.com>
2025-10-26 11:30:44 +0900

    Add commit action template

--- src/hubgenie.ts
+++ src/hubgenie.ts
@@ -1,10 +1,13 @@
 import { Command } from "@cliffy/command";
 import { META } from "./meta.ts";
+import { commitAction } from "./core/commit.ts";
 
 if (import.meta.main) {
   await new Command()
     .name(META.name)
     .description(META.description)
     .version(META.version)
+    .command("commit", "Generate some commit messages")
+    .action(commitAction)
     .parse(Deno.args);
 }
