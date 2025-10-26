38be902fd7898e5e842ae5ee55440073bf757a7b
Naatin <76672482+naatin777@users.noreply.github.com>
2025-10-26 19:35:52 +0900

    Apply format

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,18 +1,26 @@
 export interface GitRunner {
-    run(args: string[]): Promise<string>;
+  run(args: string[]): Promise<string>;
 }
 
 export class DefaultGitRunner implements GitRunner {
-    async run(args: string[]): Promise<string> {
-        const cmd = new Deno.Command("git", {
-            args,
-            stdout: "piped",
-        });
-        const output = await cmd.output();
-        return new TextDecoder().decode(output.stdout);
-    }
+  async run(args: string[]): Promise<string> {
+    const cmd = new Deno.Command("git", {
+      args,
+      stdout: "piped",
+    });
+    const output = await cmd.output();
+    return new TextDecoder().decode(output.stdout);
+  }
 }
 
-export async function getGitDiff(runner: GitRunner = new DefaultGitRunner()): Promise<string> {
-    return await runner.run(["diff", "--cached", "--unified=0", "--color=never", "--no-prefix"]);
+export async function getGitDiff(
+  runner: GitRunner = new DefaultGitRunner(),
+): Promise<string> {
+  return await runner.run([
+    "diff",
+    "--cached",
+    "--unified=0",
+    "--color=never",
+    "--no-prefix",
+  ]);
 }
