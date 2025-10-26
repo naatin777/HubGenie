61190c9a2ab324dbccbdd0ea8399a290986c90eb
Naatin <76672482+naatin777@users.noreply.github.com>
2025-10-26 11:49:48 +0900

    Add getGitDiff function

---
+++ src/utils/git.ts
@@ -0,0 +1,18 @@
+export interface GitRunner {
+    run(args: string[]): Promise<string>;
+}
+
+export class DefaultGitRunner implements GitRunner {
+    async run(args: string[]): Promise<string> {
+        const cmd = new Deno.Command("git", {
+            args,
+            stdout: "piped",
+        });
+        const output = await cmd.output();
+        return new TextDecoder().decode(output.stdout);
+    }
+}
+
+export async function getGitDiff(runner: GitRunner = new DefaultGitRunner()): Promise<string> {
+    return await runner.run(["diff", "--cached", "--unified=0", "--color=never", "--no-prefix"]);
+}
---
+++ src/utils/git_test.ts
@@ -0,0 +1,8 @@
+import { getGitDiff, GitRunner } from "./git.ts";
+import { assertEquals } from "@std/assert";
+
+Deno.test("getGitDiff", async () => {
+    const mock: GitRunner = { run: async (args) => await args.join(" ") };
+    const result = await getGitDiff(mock);
+    assertEquals(result, "diff --cached --unified=0 --color=never --no-prefix");
+});
