05dd42c5cb3f168f93f92b8dd0a2103a47ec58f7
Naatin <76672482+naatin777@users.noreply.github.com>
2025-11-15 23:58:08 +0900

    refactor: Migrate commit action to use createParsedCompletions

--- src/actions/commit.ts
+++ src/actions/commit.ts
@@ -1,7 +1,8 @@
 import { GitService } from "../git/git_service.ts";
-import { getCommitMessage } from "../utils/openai.ts";
+import { createParsedCompletions } from "../utils/openai.ts";
 import { Input } from "@cliffy/prompt";
 import { Spinner } from "../utils/spinner.ts";
+import z from "zod";
 
 export async function commitAction() {
   const spinner = new Spinner("Loading...");
@@ -9,17 +10,36 @@ export async function commitAction() {
   const git = new GitService();
   const diff = await git.diff.getGitDiffStaged();
   if (diff) {
-    console.log(diff);
-    const messages = await getCommitMessage(diff);
+    const result = await createParsedCompletions(
+      [
+        {
+          role: "system",
+          content:
+            `Act as an expert AI assistant specialized in generating Git commit messages.
+            I will provide you with a patch in the Unified Diff Format.
+            Your task is to analyze this diff and generate 10 distinct commit message suggestions that accurately describe the changes.`,
+        },
+        {
+          role: "user",
+          content: diff,
+        },
+      ],
+      z.object({ commit_message: z.array(z.string()) }),
+      "commit messages",
+    );
     spinner.stop();
-    const message = await Input.prompt({
-      message: "Enter commit messages",
-      suggestions: messages,
-      default: messages[0],
-      list: true,
-      info: true,
-    });
-    console.log(message);
+    if (result != null) {
+      const message = await Input.prompt({
+        message: "Enter commit messages",
+        suggestions: result.commit_message,
+        default: result.commit_message[0],
+        list: true,
+        info: true,
+      });
+      console.log(message);
+    } else {
+      console.log("No changes to commit");
+    }
   } else {
     spinner.stop();
     console.log("No changes to commit");
