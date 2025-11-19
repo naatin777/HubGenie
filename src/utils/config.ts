ced33f42b9756d1d0a6eefaeca5f628954ee2e63
Naatin <76672482+naatin777@users.noreply.github.com>
2025-11-19 18:26:43 +0900

    feat: migrate json to yaml (#70)
    * chore: add jsr:@std/yaml@^1.0.10 package dependency
    
    * feat: Centralize Config type definition and switch to YAML handling
    
    * feat: Implement hierarchical project config file discovery
    
    * fix: Update .gitignore to replace .hubgenie.json with .hubgenie.local.yml

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,18 +1,16 @@
 import { ConfigPaths } from "./path.ts";
 import { getEnv } from "./env.ts";
-
-export interface Config {
-  language: string;
-  editor: string;
-}
+import { parse, stringify } from "@std/yaml";
+import { Config } from "../type.ts";
 
 export async function saveConfig(
   config: Config,
   global?: true | undefined,
 ): Promise<void> {
   await ConfigPaths.ensureConfigDir(global);
-  const json = JSON.stringify(config, null, 2);
-  await Deno.writeTextFile(ConfigPaths.getConfigPath(global), json);
+  const json = stringify(config);
+  const path = await ConfigPaths.getConfigPath(global, true);
+  await Deno.writeTextFile(path, json);
 }
 
 export async function getApiKey(): Promise<string> {
@@ -29,26 +27,23 @@ export async function getModel(): Promise<string> {
 
 export async function getConfig(key: keyof Config) {
   try {
-    const localConfigFile = await Deno.readTextFile(
-      ConfigPaths.getConfigPath(undefined),
-    );
-    const localConfig: Config = JSON.parse(localConfigFile);
+    const path = await ConfigPaths.getConfigPath(undefined);
+    const localConfigFile = await Deno.readTextFile(path);
+    const localConfig: Config = parse(localConfigFile) as Config;
     return localConfig[key];
   } catch (_) {
-    const globalConfigFile = await Deno.readTextFile(
-      ConfigPaths.getConfigPath(true),
-    );
-    const globalConfig: Config = JSON.parse(
+    const path = await ConfigPaths.getConfigPath(true);
+    const globalConfigFile = await Deno.readTextFile(path);
+    const globalConfig: Config = parse(
       globalConfigFile,
-    );
+    ) as Config;
     return globalConfig[key];
   }
 }
 
 export async function getAllConfig(global?: true | undefined): Promise<Config> {
-  const configFile = await Deno.readTextFile(
-    ConfigPaths.getConfigPath(global),
-  );
-  const config: Config = JSON.parse(configFile);
+  const path = await ConfigPaths.getConfigPath(global);
+  const configFile = await Deno.readTextFile(path);
+  const config: Config = parse(configFile) as Config;
   return config;
 }
--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,8 +1,40 @@
-import { join } from "@std/path";
-import { ensureDir } from "@std/fs";
+import { dirname, join } from "@std/path";
+import { ensureDir, existsSync } from "@std/fs";
 import { META } from "../meta.ts";
 
 export class ConfigPaths {
+  private static findProjectConfig(filename: string): string {
+    let currentDir = Deno.cwd();
+
+    while (true) {
+      const filePath = join(currentDir, filename);
+      if (existsSync(filePath)) {
+        return filePath;
+      }
+
+      const parentDir = dirname(currentDir);
+      if (parentDir === currentDir) {
+        throw new Error(`${filename} not found`);
+      }
+      currentDir = parentDir;
+    }
+  }
+
+  private static async getOrCreateProjectConfig(
+    filename: string,
+    create?: boolean,
+  ): Promise<string> {
+    if (create) {
+      const currentDir = Deno.cwd();
+      const createPath = join(currentDir, filename);
+      await ensureDir(dirname(createPath));
+      await Deno.writeTextFile(createPath, "");
+      return createPath;
+    } else {
+      return this.findProjectConfig(filename);
+    }
+  }
+
   private static getGlobalConfigDir(): string {
     const home = Deno.env.get("HOME") || Deno.env.get("USERPROFILE");
     if (!home) {
@@ -11,16 +43,36 @@ export class ConfigPaths {
     return join(home, ".config", META.name);
   }
 
-  private static getGlobalConfigPath(): string {
-    return join(this.getGlobalConfigDir(), "config.json");
+  private static async getOrCreateGlobalConfigPath(): Promise<string> {
+    const createPath = join(this.getGlobalConfigDir(), "config.yml");
+    if (!existsSync(createPath)) {
+      await ensureDir(dirname(createPath));
+      await Deno.writeTextFile(createPath, "");
+    }
+    return createPath;
   }
 
-  private static getLocalConfigPath(): string {
-    return join(Deno.cwd(), `.${META.name}.json`);
+  private static async getProjectConfigPath(
+    create?: boolean,
+  ): Promise<string> {
+    const filename = `.${META.name}.yml`;
+    return await this.getOrCreateProjectConfig(filename, create);
   }
 
-  static getConfigPath(global?: true | undefined): string {
-    return global ? this.getGlobalConfigPath() : this.getLocalConfigPath();
+  private static async getLocalConfigPath(
+    create?: boolean,
+  ): Promise<string> {
+    const filename = `.${META.name}.local.yml`;
+    return await this.getOrCreateProjectConfig(filename, create);
+  }
+
+  static async getConfigPath(
+    global?: true | undefined,
+    create?: boolean,
+  ): Promise<string> {
+    return global
+      ? await this.getOrCreateGlobalConfigPath()
+      : await this.getLocalConfigPath(create);
   }
 
   static async ensureConfigDir(global?: true | undefined): Promise<void> {
