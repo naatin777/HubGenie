64230292d6ed1dc200081134f4cd893c41a4dc45
Naatin <76672482+naatin777@users.noreply.github.com>
2025-11-23 22:51:15 +0900

    feat(init): Prevent re-initialization if config exists
    Add a check within the 'init' action to determine if a configuration file already exists. If found, an error message is displayed, and the process exits, preventing accidental overwrites or re-creation of an existing configuration.

--- src/actions/init.ts
+++ src/actions/init.ts
@@ -1,10 +1,16 @@
 import type { ScopeFlag } from "../type.ts";
 import { saveConfig } from "../utils/config.ts";
 import { initEditor, initLanguage } from "../utils/selection.ts";
+import { getConfig } from "../utils/config.ts";
 
 export async function initAction(
   options: ScopeFlag,
 ): Promise<void> {
+  const config = await getConfig(options);
+  if (config) {
+    console.error("Config already exists");
+    Deno.exit(1);
+  }
   const language = await initLanguage();
   const editor = initEditor();
   await saveConfig({
