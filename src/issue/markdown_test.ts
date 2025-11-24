76b6a3343f5fafda9a85c4559fe022cc0610318e
Naatin <76672482+naatin777@users.noreply.github.com>
2025-11-24 13:40:26 +0900

    test(issue): Add test for stringifyMarkdownIssue
    Verifies that the `stringifyMarkdownIssue` function correctly serializes an issue object into a markdown string, including frontmatter.

--- src/issue/markdown_test.ts
+++ src/issue/markdown_test.ts
@@ -1,5 +1,8 @@
 import { assertEquals } from "@std/assert";
-import { parseMarkdownIssueTemplate } from "./markdown.ts";
+import {
+  parseMarkdownIssueTemplate,
+  stringifyMarkdownIssue,
+} from "./markdown.ts";
 
 Deno.test("parseMarkdownIssueTemplate", () => {
   const markdown = `---
@@ -17,4 +20,25 @@ This is the body of my issue template.
   assertEquals(template.name, "my-issue-template");
   assertEquals(template.about, "This is my issue template");
   assertEquals(template.body, "This is the body of my issue template.");
+});
+
+Deno.test("stringifyMarkdownIssue", () => {
+  const issue = {
+    title: "My Issue Template",
+    name: "my-issue-template",
+    about: "This is my issue template",
+    body: "This is the body of my issue template.",
+  };
+
+  const markdown = stringifyMarkdownIssue(issue);
+  assertEquals(
+    markdown,
+    `---
+title: My Issue Template
+
+---
+
+This is the body of my issue template.
+`,
+  );
 });
