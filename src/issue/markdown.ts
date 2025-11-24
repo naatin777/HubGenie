c9acc93122274c0316134035708bdab1f86f7627
Naatin <76672482+naatin777@users.noreply.github.com>
2025-11-24 13:30:33 +0900

    feat(issue): Implement issue to markdown serialization
    Adds the `stringifyMarkdownIssue` function to convert an `Issue` object into a markdown string. This includes generating a YAML front matter for metadata like the title, enhancing consistency and tool compatibility.

--- src/issue/markdown.ts
+++ src/issue/markdown.ts
@@ -1,4 +1,5 @@
-import type { IssueTemplate } from "../type.ts";
+import { stringify } from "@std/yaml";
+import type { Issue, IssueTemplate } from "../type.ts";
 import { extract } from "@std/front-matter/yaml";
 
 export function parseMarkdownIssueTemplate(
@@ -11,4 +12,18 @@ export function parseMarkdownIssueTemplate(
     name: attrs.name,
     about: attrs.about,
   };
+}
+
+export function stringifyMarkdownIssue(
+  issue: Issue,
+): string {
+  const yamlBlock = stringify({
+    title: issue.title,
+  });
+  return `---
+${yamlBlock}
+---
+
+${issue.body.trim()}
+`;
 }
